51098629e3f03d4c922919e41a33d96c998966a5
mohitarora249 <mohitarora249@gmail.com>
2021-12-11 09:15:26 +0530

    copy-to-clipboard functionality added

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # copy-to-clipboard-chrome-ext
 
-This chrome extension adds a copy to clipboard action to all the &lt;code> tags to the pages you visit.
+This chrome extension adds a copy to clipboard action to all the `<code>` tags to the pages you visit.
+
+# How to use this extension
+
+1. Open chrome browser
+2. Enter chrome://extensions/ in search bar.
+3. Turn on developer mode on the top right corner.
+4. Click load unpacked extension. It opens up a file picker.
+5. Select the copy-to-clipboard-chrome-ext folder.
+6. Extension is added, Just reload page and copy btn will be added to all the `<code>` tags
--- content.js
+++ content.js
@@ -1,14 +1,15 @@
 const codeBlocks = document.getElementsByTagName("code");
 
-for (elem of codeBlocks) {
+for (let i = 0; i < codeBlocks.length; i++) {
   const copyBtn = document.createElement("button");
+  copyBtn.id = i;
   copyBtn.style.marginLeft = "5px";
   copyBtn.style.marginRight = "5px";
   copyBtn.style.border = "1px solid dimgray";
   copyBtn.style.borderRadius = "10px";
   copyBtn.innerHTML = "Copy";
-  copyBtn.addEventListener("click", () =>
-    window.navigator.clipboard.writeText(elem.innerText)
-  );
-  elem.appendChild(copyBtn);
+  copyBtn.onclick = () => {
+    window.navigator.clipboard.writeText(codeBlocks[i].innerText);
+  };
+  codeBlocks[i].appendChild(copyBtn);
 }
