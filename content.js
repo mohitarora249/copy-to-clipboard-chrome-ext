1435d0f2990cfe52a3d75fb82508be51fc8fb52c
mohitarora249 <mohitarora249@gmail.com>
2021-12-15 09:00:18 +0530

    added copy toast

--- content.js
+++ content.js
@@ -1,4 +1,22 @@
 const codeBlocks = document.getElementsByTagName("code");
+const text = document.createElement("h3");
+text.innerHTML = "Copied!";
+
+const toast = document.createElement("div");
+toast.style.visibility = "hidden";
+toast.style.minWidth = "140px";
+toast.style.minHeight = "40px";
+toast.style.backgroundColor = "antiquewhite";
+toast.style.borderRadius = "10px";
+toast.style.position = "absolute";
+toast.style.bottom = "5px";
+toast.style.right = "10px";
+toast.style.zIndex = "1";
+toast.style.display = "flex";
+toast.style.justifyContent = "center";
+toast.style.alignItems = "center";
+toast.appendChild(text);
+document.body.appendChild(toast);
 
 for (let i = 0; i < codeBlocks.length; i++) {
   const copyBtn = document.createElement("button");
@@ -11,6 +29,10 @@ for (let i = 0; i < codeBlocks.length; i++) {
   copyBtn.innerHTML = "Copy";
   copyBtn.onclick = () => {
     window.navigator.clipboard.writeText(codeBlocks[i].innerText);
+    toast.style.visibility = "";
+    setTimeout(() => {
+      toast.style.visibility = "hidden";
+    }, 2000);
   };
   codeBlocks[i].parentNode.insertBefore(copyBtn, codeBlocks[i].nextSibling);
 }
